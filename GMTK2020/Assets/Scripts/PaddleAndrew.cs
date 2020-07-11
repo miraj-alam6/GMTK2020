052f406879da1b7a3a33d06c9270668142097a7f
Andrew Marshall <andrewleonmarshall@gmail.com>
2020-07-11 14:10:45 -0400

    1) Ball reflections (2) paddle basic movement

---
+++ GMTK2020/Assets/Scripts/PaddleAndrew.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleAndrew : MonoBehaviour
+{
+    public float Speed = 0f;
+    public float MaxSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    void FixedUpdate()
+    {
+        MovePaddle();
+    }
+
+    void MovePaddle()
+    {
+        Speed = Input.GetAxis("Horizontal") * Time.deltaTime * MaxSpeed;      //source to be changed
+        transform.Translate(Speed * Vector2.right);    //move relative to the paddle's current position
+    }
+}
