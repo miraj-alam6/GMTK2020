a69c0504154cffd0024424afc5a90f9f51a93654
miraj6 <miraj.saad.alam@gmail.com>
2020-07-10 23:29:55 -0400

    Fixed text mesh pro

--- GMTK2020/Assets/Scripts/GameUI.cs
+++ GMTK2020/Assets/Scripts/GameUI.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameUI : MonoBehaviour
 {
+
+    public TMP_Text Team1Label;
+   // public Team1Score
+
+
     // Start is called before the first frame update
     void Start()
     {
