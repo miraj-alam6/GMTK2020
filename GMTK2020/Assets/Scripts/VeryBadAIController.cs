1ccb47cb9a1fe578bb8841a420af38d84e702eac
miraj6 <miraj.saad.alam@gmail.com>
2020-07-12 14:06:16 -0400

    Dirty AI

--- GMTK2020/Assets/Scripts/VeryBadAIController.cs
+++ GMTK2020/Assets/Scripts/VeryBadAIController.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VeryBadAIController : MonoBehaviour
-{
+public class VeryBadAIController : MonoBehaviour{
+    public float MaxTimeBetweenRandomizers = 0.5f;
+    public float MinTimeBetweenRandomizers = 2f;
+    public float TimeLeftToReRandomize;
+    public Vector2[] InputToFeed;
+    public Team MyTeam;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Awake()
+    {       
+        InputToFeed = new Vector2[MyTeam.Paddles.Length];
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (TimeLeftToReRandomize > 0f) {
+            TimeLeftToReRandomize -= Time.deltaTime;
+            for (int i=0; i < InputToFeed.Length; i++) {
+                MyTeam.ProcessMoveInputForPaddle(InputToFeed[i], i);
+            }
+        }
+        else {
+            TimeLeftToReRandomize = Random.Range(MinTimeBetweenRandomizers, MaxTimeBetweenRandomizers);
+            for (int i = 0; i < InputToFeed.Length; i++) {
+                InputToFeed[i] = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+                MyTeam.ProcessMoveInputForPaddle(InputToFeed[i], i);
+            }
+        }
     }
 }
